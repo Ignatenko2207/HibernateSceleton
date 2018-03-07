c61c871da07e0c3184a10076c1044ccf487d3c8c
Alex Ignatenko <alex.ignatenko@scalors.com>
2018-03-07 19:32:45 +0200

    Add aditional check

--- HibernateSceleton/src/main/java/org/itstep/App.java
+++ HibernateSceleton/src/main/java/org/itstep/App.java
@@ -14,58 +14,64 @@ import org.itstep.util.HibernatePropertiesUtil;
 /**
  * @author Alex Ignatenko
  */
-public class App {
-	public static void main(String[] args) {
+public class App
+{
+	public static void main(String[] args)
+	{
 		Account account = new Account("ignatenko2207", "123456", "Alex", "Ignatenko", new Date(82, 7, 22).getTime());
 
 		Good good = new Good("BAJDHG52HG", "Some name for test good", 12599);
-		
+
 		HibernatePropertiesUtil hUtil = new HibernatePropertiesUtil();
-		
-		
-//		Session session1 = hUtil.getSessionFactory().openSession();
-//
-//		session1.getTransaction().begin();
-//
-//		session1.save(good);
-//
-//		session1.getTransaction().commit();
-//
-//		session1.close();
-		
+
+		Session session1 = hUtil.getSessionFactory().openSession();
+
+		session1.getTransaction().begin();
+
+		session1.save(good);
+
+		session1.save(account);
+
+		session1.getTransaction().commit();
+
+		session1.close();
+
 		GoodAction goodAction = new GoodAction(System.currentTimeMillis(), "try to add to cart", Boolean.FALSE, account, good);
-		
+
 		Session session2 = hUtil.getSessionFactory().openSession();
-		
+
 		session2.getTransaction().begin();
-		
+
 		session2.save(goodAction);
-		
+
 		session2.getTransaction().commit();
-		
+
 		session2.close();
-		
+
 		String sql = "SELECT account_login FROM good_actions WHERE time_action> :time";
-		
+
 		Session session3 = hUtil.getSessionFactory().openSession();
-		
+
 		session3.getTransaction().begin();
-		
-		Query query = session3.createNativeQuery(
-				sql)
-				.setParameter("time", Long.valueOf(System.currentTimeMillis()-(120*60*1000)));
-		
-		List<Account> result = query.getResultList();
-		
+
+		Query query = session3.createNativeQuery(sql);
+
+		query.setParameter("time", Long.valueOf(System.currentTimeMillis() - (15 * 60 * 1000)));
+
+		List<String> result = query.getResultList();
+
 		session3.getTransaction().commit();
-		
+
 		session3.close();
-		
 
 		System.out.println(result.size());
-		
-		System.out.println("Everything is done!");
 
+		for ( String string : result )
+		{
+			System.out.println(string);
+		}
+
+		System.out.println("Everything is done!");
 
 	}
 }
